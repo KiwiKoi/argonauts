b573a208ab3e28dc60bddb2ced82b2ac1ce0bf34
Daniel <danielo.visage@gmail.com>
2020-07-14 21:11:57 +0200

    configs

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -26,9 +26,8 @@ const membersRouter = require("./routes/members");
 app.use("/members", membersRouter);
 
 // Serve static assets in production
-if (process.env.NODE_ENV == "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
