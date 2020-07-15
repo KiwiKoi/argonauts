3b43885f3c1b5e1e736d045a1187265e607c1f4c
Daniel <danielo.visage@gmail.com>
2020-07-15 14:58:43 +0200

    updates

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const mongoose = require("mongoose");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // DB configuration
