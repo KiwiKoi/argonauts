7c58d908711c9415ebee3963e0c6529db8cf5115
Daniel <danielo.visage@gmail.com>
2020-07-15 13:06:56 +0200

    Updates

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 // DB configuration
 
-const uri = `process.env.${MONGO_URI}`;
+const uri = process.env.MONGO_URI;
 
 // connect to MongoDB
 mongoose
-  .connect(uri, {
+  .connect(`${uri}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
