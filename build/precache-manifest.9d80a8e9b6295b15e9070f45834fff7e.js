2b77015a653d9ac4795500802d76bf08b884a4b8
Daniel <danielo.visage@gmail.com>
2020-07-15 09:50:41 +0200

    Updated folder structure

---
+++ build/precache-manifest.9d80a8e9b6295b15e9070f45834fff7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9284284d5493484b6c8580269d064ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9ad9fe33d347e0d696b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6dd0fb8017948d2ce84c",
+    "url": "/static/css/main.3c2be448.chunk.css"
+  },
+  {
+    "revision": "f9ad9fe33d347e0d696b",
+    "url": "/static/js/2.1c5dafc9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1c5dafc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd0fb8017948d2ce84c",
+    "url": "/static/js/main.e236ece4.chunk.js"
+  },
+  {
+    "revision": "f4f39cf31fef5aadfa7f",
+    "url": "/static/js/runtime-main.d7a8e21c.js"
+  },
+  {
+    "revision": "43991aded877409c630ed0de305180be",
+    "url": "/static/media/logo.43991ade.png"
+  }
+]);
